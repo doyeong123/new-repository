dd1159372bf8929d42f35dc648c987a27146766b
강도영 <eyoadkgn1204@gmail.com>
2024-05-29 20:21:11 +0900

    board

---
+++ src/main/java/com/spring/board/entity/BoardEntity.java
@@ -0,0 +1,43 @@
+package com.spring.board.entity;
+
+import com.spring.board.dto.BoardDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "board_table")
+public class BoardEntity extends BaseEntity {
+
+    @Id // pk컬럼 지정. 필수
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increament
+    private Long id;
+
+    @Column(length = 20, nullable = false) // 크기 20, not null
+    private String boardWriter;
+
+    @Column // 크기 255, null 가능상태
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column(length = 500)
+    private String boardContents;
+
+    @Column
+    private int boardHits;
+
+    public static BoardEntity toSaveEntity(BoardDTO boardDTO) {
+
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(0); //조회수는 기본적으로 0이라 0으로 설정.
+        return boardEntity;
+    }
+}
